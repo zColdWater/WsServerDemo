d16ae0fb44c51e11f191d87a3db91eb8a570de90
yongpeng-zhu <yongpeng.zhu@binance.com>
2021-08-17 17:53:26 +0800

    init

---
+++ WsServer/app.js
@@ -0,0 +1,27 @@
+import { WebSocketServer } from 'ws';
+import http from 'http';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws) {
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
+  });
+
+  ws.send('something');
+});
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
